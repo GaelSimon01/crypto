421494b9b829cfca95011603a4f5c59b69d9e008
marmionsteven8 <marmionsteven8@gmail.com>
2023-12-05 17:41:23 +0100

    finition du développement de la double extension de chiffrage avec les fichiers fournies par celene

---
+++ constantes.py
@@ -0,0 +1,5 @@
+""" Est le fichier qui stocke les constantes utiles """
+DOSSIER_TXT = "celene/"
+FICHIER_TXT_ARSENE_LUPIN = DOSSIER_TXT + "arsene_lupin_extrait.txt"
+FICHIER_LETTRES_PERSANNES = DOSSIER_TXT + "lettres_persanes.txt"
+LONGUEUR_CLE_BINAIRE = 2**8 # est le chiffrement d'un octet
---
+++ convertisseur.py
@@ -0,0 +1,15 @@
+""" Est le fichier de conversion str to bin et bin to str """
+
+from gestionnaire_de_fichier import lit_le_fichier
+from constantes import FICHIER_LETTRES_PERSANNES, FICHIER_TXT_ARSENE_LUPIN
+
+def convertit_str_en_dec(message_en_str: str) -> list[str]:
+    return list(ord(c) for c in message_en_str)
+
+arsene_lupin = lit_le_fichier(FICHIER_LETTRES_PERSANNES)
+print(convertit_str_en_dec(arsene_lupin))
+
+print("\n# ================================================================ #\n")
+
+lettres_persannes = lit_le_fichier(FICHIER_TXT_ARSENE_LUPIN)
+print(convertit_str_en_dec(lettres_persannes))
