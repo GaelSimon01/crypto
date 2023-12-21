2a6a67983fb60fc2c01ddf1ebf60d7615a971982
stevenMarmion <marmionsteven7@gmail.com>
2023-12-21 09:34:33 +0100

    ajout de la lecture des images et des différence de pixels entre les deux images

--- src/gestionnaire_de_fichier.py
+++ src/gestionnaire_de_fichier.py
@@ -1,3 +1,7 @@
+from constantes import DOSSIER_SRC
+
+import os
+
 def lit_le_fichier(nom_du_fichier: str) -> None:
     try:
         fichier = open(nom_du_fichier, "r")
@@ -6,3 +10,12 @@ def lit_le_fichier(nom_du_fichier: str) -> None:
     contenu = fichier.read()
     fichier.close()
     return contenu
+
+def ecris_dans_un_fichier(nom_du_fichier: str, datas: list[str]) -> None:
+    f = open("./src/" + nom_du_fichier, 'w')
+    f.write(str(datas))
+    print('on écrit dans ' + nom_du_fichier)
+
+def efface_un_fichier(nom_du_fichier: str) -> None:
+    os.remove(nom_du_fichier)
+    print('on détruit ' + nom_du_fichier)
